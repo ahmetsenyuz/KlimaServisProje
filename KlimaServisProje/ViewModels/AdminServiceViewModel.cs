370988974d789db3bf63821c80df961b3cd14aa1
BurakOzyolu <burakozyolu@gmail.com>
2022-03-04 18:52:00 +0300

    .

--- KlimaServisProje/ViewModels/AdminServiceViewModel.cs
+++ KlimaServisProje/ViewModels/AdminServiceViewModel.cs
@@ -9,7 +9,7 @@ namespace KlimaServisProje.ViewModels
         public string AcInfo { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
-        [Range(0, 50000, ErrorMessage = "Lütfen Geçerli Bir Sayı Giriniz!")]
+        [Range(0, 50000, ErrorMessage = "Lütfen Geçerli Bir Sayı Giriniz")]
         public decimal TotalFee { get; set; }
         public bool FeeStatus { get; set; }
         public string TechName { get; set; }
